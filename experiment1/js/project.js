44d023a2132205781a89299e3cbf77bfe96c4b1d
Kevingallegos2003 <kevingallegos2003@gmail.com>
2025-05-27 21:27:32 -0700

    New Experiment 1

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -9,7 +9,8 @@ const fillers = {
   loots: ["alllowance", "chors", "Dishes", "Unclipped grass", "check engine lights", "Modly tub","laundry", "bananas", "noodles", "goblins", "CS Majors", "college credits"],
   baddies: ["Loansharks", "Lawyers", "fishmen", "Compsci majors", "internet trolls", "college professors", "dragon deez nuts", "evil $adventurer", "agents of chaos"],
   message: ["call", "txt", "post", "decree", "shoutz", "tweets", "choiche", "hearkens", "harkening", "harkenening", "harkenenening", "...wait, no! Come back", "Watermelon"],
-  
+  complete: ["You did it!", "Congradulations!", "well done $adventurer", "Nicely done!", "Bravo!"],
+  lazy: ["You lazy bum! You did not complete the task!", "Nice try, actually do your task nerd!", "Nice surf history $adventurer maybe you should do your task or I will show it to the world!", "Not funny didn't, laugh, do your task!"]
 };
 
 const template = `$adventurer, heed my $message!
@@ -18,12 +19,15 @@ I have just come from $pre$post where the $people folk are in desperate need. Th
 
 It is told that the one who can rescue them will be awarded with $num $looty $loots. Surely this must tempt one such as yourself!
 `;
-
+const completedTask = `$adventurer, $complete you have successfully conquered the $baddies and saved the $people, onward yo your next task!`;
+const lazy = `$lazy`;
 
 // STUDENTS: You don't need to edit code below this line.
 
 const slotPattern = /\$(\w+)/;
-
+var currency = 0;
+var completed = 0;
+var task  = false;
 function replacer(match, name) {
   let options = fillers[name];
   if (options) {
@@ -34,18 +38,42 @@ function replacer(match, name) {
 }
 
 function generate() {
+  task = true;
   let story = template;
   while (story.match(slotPattern)) {
     story = story.replace(slotPattern, replacer);
   }
 
-  /* global box */
-  //box.innerText = story;
-  $("#box").text(story);
+  /* global box, counter */
+  box.innerText = story;
+  counter.innerText = `Tasks completed: ${completed} ------------------------------ Productivity Coins: ${currency}`
+}
+function completeTask(){
+  if(task){
+    task = false;
+    if(Math.floor(Math.random()*4) < 2){
+      let story = completedTask;
+      while (story.match(slotPattern)) {
+      story = story.replace(slotPattern, replacer);
+      }
+      box.innerText = story;
+      completed++;
+      currency += Math.floor(Math.random()*20+1);
+      counter.innerText = `Tasks completed: ${completed} ------------------------------ Productivity Coins: ${currency}`;
+    }
+    else{
+      let story = lazy;
+      while (story.match(slotPattern)) {
+      story = story.replace(slotPattern, replacer);
+      }
+      box.innerText = story;
+    }
+  }
 }
 
-/* global clicker */
-//clicker.onclick = generate;
-$("#clicker").click(generate);
+/* global clicker, complete */
+clicker.onclick = generate;
+complete.onclick = completeTask;
+
 
 generate();
